c3307058a1674409415a0c415bd9c92609239f13
JBlueCode <bluemtjones@gmail.com>
2024-02-01 21:43:42 -0600

    Week-1

---
+++ Assets/Week_2/Scpit/InterfaceManager.cs
@@ -0,0 +1,3 @@
+using System.Collections;using System.Collections.Generic;using TMPro;using UnityEngine;public class InterfaceManager : MonoBehaviour{    public TextMeshProUGUI label;    public void PrintMessage(string msg)    {        label.text = msg;    }    public void Add(int number)    {        label.text = (number + 10).ToString();    }
+
+}
